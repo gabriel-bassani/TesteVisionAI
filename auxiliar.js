68d8e4551cab57f3b00086e45aea2c64d0b39710
Bassani <gabrielbassabarreto@gmail.com>
2022-02-24 21:54:27 -0300

    correcoes

--- auxiliar.js
+++ auxiliar.js
@@ -3,6 +3,18 @@ const vision = require("@google-cloud/vision");
 const client = new vision.ImageAnnotatorClient({
   keyFilename: "D:\\Documentos_HDD\\testeai-339522-04098fdf05ed.json",
 });
+client
+  .documentTextDetection("C:\\Users\\Gabriel\\Pictures\\Leviathan_Wakes.jpg")
+  .then((results) => {
+    const texts = results[0].textAnnotations;
+
+    console.log("Texts:");
+    texts.forEach((text) => console.log(text.description));
+  })
+  .catch((err) => {
+    console.error("ERROR:", err);
+  });
+
 /*
 client
   .labelDetection("C:\\Users\\Gabriel\\Pictures\\0_7jYE1-Za4AzaJdru.jfif")
@@ -16,14 +28,3 @@ client
     console.error("ERROR:", err);
   });
 */
-client
-  .documentTextDetection("C:\\Users\\Gabriel\\Pictures\\Leviathan_Wakes.jpg")
-  .then((results) => {
-    const texts = results[0].textAnnotations;
-
-    console.log("Texts:");
-    texts.forEach((text) => console.log(text.description));
-  })
-  .catch((err) => {
-    console.error("ERROR:", err);
-  });
